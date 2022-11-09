1a6eb696cc7b5b9ab878e8df395ccbc4c714efe8
NobunagaOda007 <leonardo.puhanic2@gmail.com>
2022-11-09 19:34:23 +0100

    .

--- index.js
+++ index.js
@@ -33,7 +33,9 @@ app.use(session({
     secret: 'some secret',
     cookie: { maxAge: 100*100*60*60*12,
             secure: true,
-        sameSite: "none"},
+            httpOnly:false,
+        sameSite: "none",
+        domain:'https://master--vermillion-zabaione-03d5a9.netlify.app'},
     saveUninitialized: false,
     resave: true,
     store,
@@ -50,7 +52,7 @@ app.use(express.json());
 app.use(cors({
     origin: [
         'http://localhost:5000',
-   //     'https://master--vermillion-zabaione-03d5a9.netlify.app',
+        'https://master--vermillion-zabaione-03d5a9.netlify.app',
         'http://localhost:5173',
     ],
     
@@ -61,7 +63,7 @@ app.use(cors({
 app.use(Router);
 
 app.use(function(req,res,next){
-   // res.header('Access-Control-Allow-Origin', "https://master--vermillion-zabaione-03d5a9.netlify.app");
+    res.header('Access-Control-Allow-Origin', "https://master--vermillion-zabaione-03d5a9.netlify.app");
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
